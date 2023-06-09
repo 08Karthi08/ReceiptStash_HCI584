76778d35cde086331c316e7760838ba35696b597
Karthika Suresh Kumar <karthika@iastate.edu>
2023-06-08 23:55:02 -0500

    OCR scanner added
    Scanner is did not retrieve some text elements like tip. Need to fix it.

--- ReceiptScanner/ocrscanner.py
+++ ReceiptScanner/ocrscanner.py
@@ -4,5 +4,5 @@ import pytesseract
 def ocr_core(img):
     text = pytesseract.image_to_string(img)
     return text
-img = cv2.imread('/Users/karthi/Documents/584/Git/ReceiptStash/ReceiptScanner/img.jpeg')
+img = cv2.imread('/Users/karthi/Documents/584/Git/ReceiptStash_HCI584/ReceiptScanner/img.jpeg')
 print(ocr_core(img))
