75aecbab010e0c51cde0bbe0bc6ea0a0f84a010c
Karthika Suresh Kumar <karthika@iastate.edu>
2023-06-08 23:19:18 -0500

    OCR scanner added.
    The scanner is not perfect. tip part of the receipt was not read properly.

---
+++ ReceiptScanner/ocrscanner.py
@@ -0,0 +1,8 @@
+import cv2
+import pytesseract
+
+def ocr_core(img):
+    text = pytesseract.image_to_string(img)
+    return text
+img = cv2.imread('/Users/karthi/Documents/584/Git/ReceiptStash/ReceiptScanner/img.jpeg')
+print(ocr_core(img))
