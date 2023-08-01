2cbb5a05782fda4c50edc2c68fd71e72402806ce
Karthika Suresh Kumar <karthika@iastate.edu>
2023-08-01 18:42:27 -0500

    added in-code dev guide

--- app.py
+++ app.py
@@ -1,4 +1,35 @@
-# Import necessary modules and libraries
+"""
+Flask web application for managing receipts.
+
+This application allows users to upload receipts, view, edit, delete them.
+Receipts are stored in an SQLite database, and the application provides the option to download receipt data in CSV format.
+
+Modules:
+    - flask: The Flask web framework.
+    - flask_table: A package for generating HTML tables in Flask.
+    - datetime: Date and time-related functionalities.
+    - os: Operating system-related functionalities.
+    - ocr: Optical Character Recognition (OCR) module for text extraction from images.
+    - receipt_db: Module for managing the SQLite database for receipts.
+    - date_utils: Utility functions for working with date formats.
+
+Classes:
+    - ReceiptTable: A subclass of FlaskTable used to display receipt data in an HTML table.
+
+Functions:
+    - home(): Redirects users to the 'application' route.
+    - application(): Handles the main application route and displays receipts in a table.
+    - edit_receipt(receipt_id): Handles the 'edit' route for editing a specific receipt.
+    - delete_receipt(receipt_id): Handles the 'delete' route for deleting a specific receipt.
+    - download_table(): Generates a CSV file and allows users to download all receipts.
+
+Helper Functions:
+    - convert_date_format(date_str): Converts a date from 'YYYY-MM-DD' to 'DD/MM/YYYY' format.
+    - filter_receipts_by_date(receipts, start_date, end_date): Filters receipts based on the selected date range.
+"""
+
+# The rest of the code remains unchanged.
+
 from flask import Flask, render_template, request, redirect, url_for, make_response, flash
 from flask_table import Table, Col, LinkCol
 from datetime import datetime
@@ -7,18 +38,14 @@ from ocr import OCR
 from receipt_db import Receipt, ReceiptDB
 from date_utils import DateConverter
 
-# Create a Flask web application
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Set the secret key for session security
+app.secret_key = 'your_secret_key'
 
-# Create a database instance and create the table if it doesn't exist
 db = ReceiptDB('receipts.db')
 db.create_table()
 
-# Define the ReceiptTable class for displaying receipts in a table format
 class ReceiptTable(Table):
-    # Define table columns and attributes
-    """A table class for displaying receipts in a tabular format."""
+    # id = Col('Receipt ID')
     
     vendor_name = Col('Vendor Name', allow_sort=True)
     date = Col('Date', allow_sort=True)
@@ -26,7 +53,6 @@ class ReceiptTable(Table):
     edit = LinkCol('Edit', 'edit_receipt', url_kwargs=dict(receipt_id='id'), attr='id', allow_sort=False, anchor_attrs={'class': 'btn btn-secondary'})
     delete = LinkCol('Delete', 'delete_receipt', url_kwargs=dict(receipt_id='id'), attr='id', allow_sort=False, anchor_attrs={'class': 'btn btn-danger', 'onclick': 'return confirmDelete(\'id\')'})
 
-    # Define a method to generate URLs for sorting columns
     def sort_url(self, col_key, reverse=False):
         if reverse:
             direction = 'desc'
@@ -37,16 +63,13 @@ class ReceiptTable(Table):
     # Add Bootstrap class attributes to table elements
     classes = ['table', 'table-striped', 'table-bordered', 'table-hover']
 
-# Define the route for the home page (root URL)
 @app.route('/')
 def home():
-    return redirect(url_for('application'))  # Redirect to the main application page
- """Redirect to the main application page."""
- 
-# Define the route for the main application page
+    return redirect(url_for('application'))
+
+
 @app.route('/app', methods=['GET', 'POST'])
 def application():
-    # Handling file uploads and the OCR processing when the form is submitted
     if request.method == 'POST':
         if 'file' not in request.files:
             return 'No file uploaded', 400
@@ -55,19 +78,17 @@ def application():
         if file.filename == '':
             return 'No file selected', 400
         else:
+            print("I am after")
             file_bytes = file.read()  # Read the file content as bytes
 
-            # Extract text, vendor name, date, and total amount using OCR
             extracted_text = OCR.extract_text_from_bytes(file_bytes)
             total_amount = OCR.extract_total_amount(extracted_text)
             vendor_name = OCR.extract_vendor_name(extracted_text)
             date = OCR.extract_date(extracted_text)
 
-            # Display a flash message if OCR extraction fails
-            if not (extracted_text or total_amount or vendor_name):
+            if(not(extracted_text or total_amount or vendor_name)):
                 flash('Image is not Supported')
-            else:
-                # Create a Receipt object and insert it into the database
+            else:    
                 receipt = Receipt({
                     0: None,
                     1: vendor_name,
@@ -75,9 +96,10 @@ def application():
                     3: total_amount
                 })
                 db.insert_receipt(receipt)
-                flash('Upload successful')  # Display a success flash message
+                # Clear the form data to prevent resubmission on refresh
+                flash('Upload successful')
 
-            return redirect(request.url)  # Redirect back to the application page
+            return redirect(request.url)
 
     # Retrieve the start and end dates from the query parameters
     start_date_str = request.args.get('start_date')
@@ -92,18 +114,15 @@ def application():
     # Filter the receipts based on the selected date range
     filtered_receipts = filter_receipts_by_date(receipts, start_date, end_date)
 
-    # Create a ReceiptTable object with the filtered receipts
-    table = ReceiptTable(filtered_receipts)
+    table = ReceiptTable(filtered_receipts)  # Pass the filtered receipts to the table object
     table.border = True  # Optionally, set a border for the table
 
-    # Render the application page with the table and date filters
     return render_template('app.html', table=table, start_date=start_date_str, end_date=end_date_str)
 
-# Define the route for editing a receipt
+
 @app.route('/edit/<int:receipt_id>', methods=['GET', 'POST'])
 def edit_receipt(receipt_id):
     if request.method == 'POST':
-        # Update receipt information based on the submitted form data
         vendor_name = request.form.get('vendor_name')
         date = request.form.get('date')
         total_amount = request.form.get('total_amount')
@@ -113,19 +132,19 @@ def edit_receipt(receipt_id):
 
         db.update_receipt(receipt_id, vendor_name, date, total_amount)
 
-        return redirect(url_for('application'))  # Redirect back to the application page
+        return redirect(url_for('application'))
 
     receipt = db.get_receipt(receipt_id)
-    return render_template('edit.html', receipt=receipt)  # Render the edit receipt page
+    return render_template('edit.html', receipt=receipt)
 
-# Define the route for deleting a receipt
+
 @app.route('/delete/<int:receipt_id>', methods=['GET', 'POST'])
 def delete_receipt(receipt_id):
     if request.method == 'GET':
-        db.delete_receipt(receipt_id)  # Delete the receipt from the database
-        return redirect(url_for('application'))  # Redirect back to the application page
+        db.delete_receipt(receipt_id)
+        return redirect(url_for('application'))
 
-# Define the route for downloading the receipt table as a CSV file
+
 @app.route('/download/table')
 def download_table():
     receipts = db.get_all_receipts()
@@ -133,7 +152,7 @@ def download_table():
         return 'No receipts found', 404
 
     # Generate the table data as a CSV file
-    csv_data = "Receipt ID,Vendor Name,Date,Total Amount\n"
+    csv_data = "Receipt ID,Image Path,Vendor Name,Date,Total Amount\n"
     for receipt in receipts:
         csv_data += "{},{},{},{}\n".format(receipt.id, receipt.vendor_name, receipt.date, receipt.total_amount)
 
@@ -144,13 +163,14 @@ def download_table():
 
     return response
 
-# Helper function to convert date format from 'YYYY-MM-DD' to 'DD/MM/YYYY'
+from datetime import datetime
+
 def convert_date_format(date_str):
+    # Convert date from 'YYYY-MM-DD' to 'DD/MM/YYYY' format
     date_obj = datetime.datetime.strptime(date_str, '%Y-%m-%d').date()
     formatted_date = date_obj.strftime('%Y-%m-%d')
     return formatted_date
 
-# Helper function to filter receipts by date range
 def filter_receipts_by_date(receipts, start_date, end_date):
     start_date = DateConverter.convert_to_date(start_date)
     end_date = DateConverter.convert_to_date(end_date)
@@ -172,6 +192,5 @@ def filter_receipts_by_date(receipts, start_date, end_date):
 
     return filtered_receipts
 
-# Run the Flask application
 if __name__ == '__main__':
     app.run(port=5001, debug=True)
